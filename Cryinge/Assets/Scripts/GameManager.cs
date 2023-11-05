666c35b3c0c30add450498abd11dcb40d0c38180
rylanc9 <rylanc9@hotmail.com>
2023-11-04 21:33:39 -0400

    EnemyTwoAdded

--- Cryinge/Assets/Scripts/GameManager.cs
+++ Cryinge/Assets/Scripts/GameManager.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject enemyOnePrefab;
+    public GameObject enemyTwoPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("CreateEnemyOne", 1.0f, 3.0f);
+        InvokeRepeating("CreateEnemyTwo", 1.0f, 5.0f);
     }
 
     // Update is called once per frame
@@ -21,5 +23,10 @@ public class GameManager : MonoBehaviour
     void CreateEnemyOne()
     {
         Instantiate(enemyOnePrefab, new Vector3(Random.Range(-8, 8), 7, 0), Quaternion.identity);
+    }
+
+    void CreateEnemyTwo()
+    {
+        Instantiate(enemyTwoPrefab, new Vector3(Random.Range(-8, 8), 7, 0), Quaternion.identity);
     }
 }
